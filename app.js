3e714b6fba16c74c51f87e6da895744d7997e580
AniaLaz <lazarenko.ania.sg@gmail.com>
2023-03-17 11:50:13 +0200

    bugfix

--- app.js
+++ app.js
@@ -15,11 +15,10 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.use("/api/users", authRouter);
 app.use("/api/contacts", contactsRouter);
-
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
@@ -27,9 +26,7 @@ app.use((req, res) => {
 
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
-  console.log("5");
   res.status(status).json({ message });
 });
 
 module.exports = app;
-
